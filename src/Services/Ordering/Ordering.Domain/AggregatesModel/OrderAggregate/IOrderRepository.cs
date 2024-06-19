83efaaff19abbf44cda15411f8bffd64f90068c1
Jilian Cappi <jilian.cappi@hotmail.com>
2024-06-19 18:59:29 -0300

    order: added domain layer (DDD)

---
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ordering.Domain.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Order Add(Order order);
+
+        void Update(Order order);
+
+        Task<Order> GetAsync(int orderId);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderStartedDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Ordering.Domain.AggregatesModel.OrderAggregate;
+
+namespace Ordering.Domain.Events;
+
+/// <summary>
+/// Event used when an order is created
+/// </summary>
+public record class OrderStartedDomainEvent(
+    Order Order,
+    string UserId,
+    string UserName,
+    int CardTypeId,
+    string CardNumber,
+    string CardSecurityNumber,
+    string CardHolderName,
+    DateTime CardExpiration) : INotification;
