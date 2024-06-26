ef3cc7886e1832676672a46f97adffc635b5b113
PedroTocantins <pedrotocantins11s@gmail.com>
2024-06-25 23:41:25 -0300

    add routerLinks on header

--- src/Web/eShop.Web/src/app/shared/header/header.component.ts
+++ src/Web/eShop.Web/src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';  
+import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
