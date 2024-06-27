6784b1e3e8ed2f20169ee90b957e670c2ac6ebea
Lucas Freitas <lcffreitas1@gmail.com>
2024-06-27 00:48:40 -0300

    base footer

--- src/Web/eShop.Web/src/app/shared/footer/footer.component.ts
+++ src/Web/eShop.Web/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSquareFacebook, faTwitterSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faSquareFacebook = faSquareFacebook;
+  faTwitterSquare = faTwitterSquare;
+  faYoutubeSquare = faYoutubeSquare;
 }
