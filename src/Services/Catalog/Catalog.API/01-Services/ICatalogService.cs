3d7a0984960f7bb24816ba9f64f76c72a15e68be
Jilian Cappi <jilian.cappi@hotmail.com>
2024-07-22 13:42:40 -0300

    catalog: added get filter (#15)

--- src/Services/Catalog/Catalog.API/01-Services/ICatalogService.cs
+++ src/Services/Catalog/Catalog.API/01-Services/ICatalogService.cs
@@ -7,23 +7,48 @@ namespace Catalog.API.Services
 {
     public interface ICatalogService
     {
-        IEnumerable<CatalogItemDTO> GetAll();
+        CatalogItemDataResult GetAll(CatalogItemFilter filter);
     }
 
     public class CatalogService(ApplicationDataContext context) : ICatalogService
     {
         private readonly ApplicationDataContext _context = context;
 
-        public IEnumerable<CatalogItemDTO> GetAll()
+        public CatalogItemDataResult GetAll(CatalogItemFilter filter)
         {
-            return _context.CatalogItems.Select(item => new CatalogItemDTO
-            {
-                Id = item.Id,
-                Name = item.Name,
-                Description = item.Description,
-                Brand = item.Brand.Name,
-                Price = item.Price
-            }).AsNoTracking();
+            filter ??= new CatalogItemFilter() { PageIndex = 0, PageSize = 10, ShowOnlyHighlighted = true };
+
+            var totalItems = _context.CatalogItems.Count(w => w.IsHighlighted == filter.ShowOnlyHighlighted);
+
+            var data = _context.CatalogItems
+                .Where(w => w.IsHighlighted == filter.ShowOnlyHighlighted)
+                .Skip(filter.PageIndex * filter.PageSize)
+                .Take(filter.PageSize)
+                .AsNoTracking()
+                .Select(item => new CatalogItemDTO
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Description = item.Description,
+                    Brand = item.Brand.Name,
+                    Price = item.Price
+                });
+
+            return new CatalogItemDataResult() { TotalItems = totalItems, Items = data };
         }
     }
+
+    public class CatalogItemFilter
+    {
+        public bool ShowOnlyHighlighted { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+
+    public class CatalogItemDataResult
+    {
+        public int TotalItems { get; set; }
+        public IEnumerable<CatalogItemDTO> Items { get; set; }
+    }
 }
+
--- tests/Catalog.FunctionalTests/Catalog.UnitTests/Services/CatalogServiceTests.cs
+++ tests/Catalog.FunctionalTests/Catalog.UnitTests/Services/CatalogServiceTests.cs
@@ -21,9 +21,9 @@ namespace Catalog.UnitTests.Services
             // Arrange
             var catalogItems = new List<CatalogItem>
             {
-                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 1", Description = "Description 1", Price = 10.99m, Brand = new CatalogBrand { Id = 1, Name = "Brand 1" } },
-                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 2", Description = "Description 2", Price = 20.99m, Brand = new CatalogBrand { Id = 2, Name = "Brand 2" } },
-                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 3", Description = "Description 3", Price = 30.99m, Brand = new CatalogBrand { Id = 3, Name = "Brand 3" } }
+                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 1", Description = "Description 1", Price = 10.99m, IsHighlighted = true, Brand = new CatalogBrand { Id = 1, Name = "Brand 1" } },
+                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 2", Description = "Description 2", Price = 20.99m, IsHighlighted = true, Brand = new CatalogBrand { Id = 2, Name = "Brand 2" } },
+                new CatalogItem { Id = Guid.NewGuid(), Name = "Item 3", Description = "Description 3", Price = 30.99m, IsHighlighted = true, Brand = new CatalogBrand { Id = 3, Name = "Brand 3" } }
             };
 
             var mockContext = GetMock<CatalogItem, ApplicationDataContext>(catalogItems, x => x.CatalogItems);
@@ -32,15 +32,15 @@ namespace Catalog.UnitTests.Services
             var catalogService = new CatalogService(mockContext);
 
             // Act
-            var result = catalogService.GetAll();
+            var result = catalogService.GetAll(It.IsAny<CatalogItemFilter>());
 
             // Assert
-            Assert.AreEqual(catalogItems.Count, result.Count());
-            Assert.AreEqual(catalogItems[0].Id, result.First().Id);
-            Assert.AreEqual(catalogItems[0].Name, result.First().Name);
-            Assert.AreEqual(catalogItems[0].Description, result.First().Description);
-            Assert.AreEqual(catalogItems[0].Brand.Name, result.First().Brand);
-            Assert.AreEqual(catalogItems[0].Price, result.First().Price);
+            Assert.AreEqual(catalogItems.Count, result.TotalItems);
+            Assert.AreEqual(catalogItems[0].Id, result.Items.First().Id);
+            Assert.AreEqual(catalogItems[0].Name, result.Items.First().Name);
+            Assert.AreEqual(catalogItems[0].Description, result.Items.First().Description);
+            Assert.AreEqual(catalogItems[0].Brand.Name, result.Items.First().Brand);
+            Assert.AreEqual(catalogItems[0].Price, result.Items.First().Price);
         }
 
 
@@ -54,9 +54,11 @@ namespace Catalog.UnitTests.Services
             dbSetMock.As<IQueryable<TData>>().Setup(s => s.Expression).Returns(lstDataQueryable.Expression);
             dbSetMock.As<IQueryable<TData>>().Setup(s => s.ElementType).Returns(lstDataQueryable.ElementType);
             dbSetMock.As<IQueryable<TData>>().Setup(s => s.GetEnumerator()).Returns(() => lstDataQueryable.GetEnumerator());
+
             dbSetMock.Setup(x => x.Add(It.IsAny<TData>())).Callback<TData>(lstData.Add);
             dbSetMock.Setup(x => x.AddRange(It.IsAny<IEnumerable<TData>>())).Callback<IEnumerable<TData>>(lstData.AddRange);
             dbSetMock.Setup(x => x.Remove(It.IsAny<TData>())).Callback<TData>(t => lstData.Remove(t));
+
             dbSetMock.Setup(x => x.RemoveRange(It.IsAny<IEnumerable<TData>>())).Callback<IEnumerable<TData>>(ts =>
             {
                 foreach (var t in ts) { lstData.Remove(t); }
@@ -69,3 +71,4 @@ namespace Catalog.UnitTests.Services
         }
     }
 }
+
