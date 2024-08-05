c332fc67ec0d3be97ea6e95357b6a16d0573ce49
Aron <aron.cardoso1@gmail.com>
2024-08-05 15:27:47 -0300

    web: updated product card

--- src/Web/eShop.Web/tailwind.config.js
+++ src/Web/eShop.Web/tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
       colors: {
         'darkblue' : '#005B96'
       },
-      fontSize:{
-        
+      screens: {
+        'mm' : '375px',
+        'ml' : '425px'
       },
+      spacing: {
+        '34' : '9.3rem' 
+      }
     },
   },
   plugins: [],
