09ae323f9e3677b7aedb2ba6fc3fa8729c6a9687
Victoria Tassi <122395427+Tassizinha@users.noreply.github.com>
2024-08-28 07:28:26 -0300

    Alterações sigin-page\registro (#31)
    added sign-in

--- src/Web/eShop.Web/src/app/components/authentication/signin-page/signin-page.component.ts
+++ src/Web/eShop.Web/src/app/components/authentication/signin-page/signin-page.component.ts
@@ -1,18 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { cpfValidator } from '../signin-page/validators'
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signin-page',
+  selector: 'app-registro-usuarios',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggleModule, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule, 
+    NgIf, 
+   ],
+
   templateUrl: './signin-page.component.html',
   styleUrl: './signin-page.component.css'
 })
-export class SigninPageComponent {
 
-  constructor(private router: Router) {}
+export class SigninPageComponent implements OnInit{
+  registrationForm!: FormGroup;
   
-  goToLoginPage() {
-    this.router.navigate(['/login']);
+  constructor(private fb: FormBuilder, private http: HttpClient,) {  }
+  
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      cpf: ['',[  Validators.required, cpfValidator()]], 
+      email: ['', [Validators.required, Validators.email]],
+      adress: ['', Validators.required],
+      cep: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+consultCep(): void {
+  const cep = this.registrationForm.get('cep')?.value;
+  if (cep && cep.length === 8) {
+    this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((data: any) => {
+      if (data.erro) {
+        this.registrationForm.get('cep')?.setErrors({ invalidCep: true });
+      } else {
+        this.registrationForm.patchValue({
+          adress: data.logradouro
+        });
+      }
+    }, () => {
+      this.registrationForm.get('cep')?.setErrors({ invalidCep: true });
+    });
   }
 }
+
+cepValidator(control: any) {
+  const cep = control.value;
+  if (cep && cep.length === 8) {
+    return null;
+  }
+  return { invalidCep: true };
+}
+
+  onSubmit(): void {
+    if (this.registrationForm.valid){
+      console.log(this.registrationForm.value);
+
+    }else{ 
+      console.log('Form is invalid');
+    }
+  }
+}
+
