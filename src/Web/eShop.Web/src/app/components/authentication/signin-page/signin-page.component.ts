613fb98c02c3dae519103b251accb33f982ed981
sofiabucci <135064015+sofiabucci@users.noreply.github.com>
2024-08-30 11:46:53 +0100

    autenticação e cadastro de user (#34)
    * autenticação e cadastro de user
    
    ---------
    
    Co-authored-by: sofiabucci <soliveirabucci@gmail.com~>

--- src/Web/eShop.Web/src/app/components/authentication/signin-page/signin-page.component.ts
+++ src/Web/eShop.Web/src/app/components/authentication/signin-page/signin-page.component.ts
@@ -9,34 +9,36 @@ import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { cpfValidator } from '../signin-page/validators'
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/authentication/auth.service';
+
 
 @Component({
   selector: 'app-registro-usuarios',
   standalone: true,
   imports: [
-    MatSlideToggleModule, 
-    MatCardModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
-    ReactiveFormsModule, 
-    NgIf, 
+    MatSlideToggleModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgIf,
    ],
-
   templateUrl: './signin-page.component.html',
-  styleUrl: './signin-page.component.css'
+  styleUrl: './signin-page.component.css',
 })
 
 export class SigninPageComponent implements OnInit{
   registrationForm!: FormGroup;
-  
-  constructor(private fb: FormBuilder, private http: HttpClient,) {  }
-  
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private authService : AuthService, private router: Router) {  }
+
   ngOnInit(): void {
     this.registrationForm = this.fb.group({
       name: ['', Validators.required],
-      cpf: ['',[  Validators.required, cpfValidator()]], 
+      cpf: ['',[  Validators.required, cpfValidator()]],
       email: ['', [Validators.required, Validators.email]],
-      adress: ['', Validators.required],
+      address: ['', Validators.required],
+      numCasa: ['', Validators.required],
       cep: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', Validators.required]
@@ -48,36 +50,48 @@ export class SigninPageComponent implements OnInit{
       ? null : { mismatch: true };
   }
 
-consultCep(): void {
-  const cep = this.registrationForm.get('cep')?.value;
-  if (cep && cep.length === 8) {
-    this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((data: any) => {
-      if (data.erro) {
+  consultCep(): void {
+    const cep = this.registrationForm.get('cep')?.value;
+    if (cep && cep.length === 8) {
+      this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe((data: any) => {
+        if (data.erro) {
+          this.registrationForm.get('cep')?.setErrors({ invalidCep: true });
+        } else {
+          this.registrationForm.patchValue({
+            adress: data.logradouro
+          });
+        }
+      }, () => {
         this.registrationForm.get('cep')?.setErrors({ invalidCep: true });
-      } else {
-        this.registrationForm.patchValue({
-          adress: data.logradouro
-        });
-      }
-    }, () => {
-      this.registrationForm.get('cep')?.setErrors({ invalidCep: true });
-    });
-  }
-}
+      });
+    }
 
-cepValidator(control: any) {
-  const cep = control.value;
-  if (cep && cep.length === 8) {
-    return null;
   }
-  return { invalidCep: true };
-}
 
-  onSubmit(): void {
+  cepValidator(control: any) {
+    const cep = control.value;
+    if (cep && cep.length === 8) {
+      return null;
+    }
+    return { invalidCep: true };
+  }
+
+  signin() {
     if (this.registrationForm.valid){
-      console.log(this.registrationForm.value);
 
-    }else{ 
+      const {name, password, email, address, cep, cpf, numCasa} = this.registrationForm.value;
+      const fullAddress = `${address}, ${numCasa}`;
+
+      this.authService.signin(name, password, email, fullAddress, cep, cpf).subscribe(
+        () => {
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error to register', error);
+        }
+      );
+
+    }else{
       console.log('Form is invalid');
     }
   }
