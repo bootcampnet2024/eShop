64cbfac8b464a520eb7b4aeda1264771cc070732
sofiabucci <soliveirabucci@gmail.com~>
2024-09-10 10:52:47 -0300

    auth service

--- src/Web/eShop.Web/src/app/services/authentication/auth.service.ts
+++ src/Web/eShop.Web/src/app/services/authentication/auth.service.ts
@@ -1,3 +1,4 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -23,10 +24,12 @@ export class AuthService {
   private adminUrl = 'http://localhost:8070/realms/eshop/protocol/openid-connect/token';
   private registerUrl = 'http://localhost:8070/admin/realms/eshop/users';
   private userclientId = 'account-user';
+  private managerclientId = 'account-manager';
+  private adminclientId = 'admin-cli';
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  private getToken(client_id : string, grant_type: string, username : string, password : string, url : string) : Observable<AuthResponse> {
+  getToken(client_id : string, grant_type: string, username : string, password : string, url : string) : Observable<AuthResponse> {
     const body = new URLSearchParams();
     body.set('client_id', client_id);
     body.set('grant_type', grant_type);
@@ -46,12 +49,44 @@ export class AuthService {
   }
 
   login(username: string, password: string): Observable<AuthResponse> {
-    return this.getToken(this.userclientId, 'password', username, password, this.adminUrl);
+    let client_id: string;
+
+    if (username.includes('manager')) {
+      client_id = this.managerclientId;
+    } else if (username.includes('admin')) {
+      client_id = this.adminclientId;
+    } else {
+      client_id = this.userclientId;
+    }
+    return this.getToken(client_id, 'password', username, password, this.adminUrl)
+    .pipe(
+      tap((response: AuthResponse) => {
+        const helper = new JwtHelperService();
+        const decodedToken = helper.decodeToken(response.access_token);
+
+        const roles = decodedToken?.realm_access?.roles;
+
+        if (!roles || roles.length === 0) {
+          console.error('No roles found in token');
+          return;
+        }
+
+        if (roles.includes('user-manager')) {
+          this.router.navigate(['/user-management']);
+        } else if (roles.includes('product-manager')) {
+          this.router.navigate(['/product-management']);
+        } else if (roles.includes('user')) {
+          this.router.navigate(['/user']);
+        } else if (roles.includes('admin')) {
+          this.router.navigate(['/admin']);
+        }
+      })
+    );
 
   }
 
   signin(username: string, password: string, email: string, address: string, cep: string, cpf: string): Observable<any> {
-    return this.getToken('admin-cli', 'password', 'admin', 'admin', this.adminUrl)
+    return this.getToken(this.adminclientId, 'password', 'admin', 'admin', this.adminUrl)
     .pipe(
       switchMap((response: AuthResponse) => {
         const token = response.access_token;
