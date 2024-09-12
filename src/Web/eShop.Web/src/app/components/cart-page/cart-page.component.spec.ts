0796fbe5f53e2912947c9b474582ecbf638b77cc
Eduardo Timossi Caetano <149100621+EduardoTCaetano@users.noreply.github.com>
2024-09-12 07:33:17 -0300

    added basket (#41)
    * Subindo back+front
    
    * subindo correções
    
    * Delete package-lock.json
    
    * Delete src/Web/package-lock.json
    
    * Update appsettings.json
    
    * Delete src/Web/eShop.Web/package-lock.json
    
    * Update appsettings.json
    
    ---------
    
    Co-authored-by: Pedro H. Garbim <133874619+pedrohgarbim@users.noreply.github.com>

--- src/Web/eShop.Web/src/app/components/cart-page/cart-page.component.spec.ts
+++ src/Web/eShop.Web/src/app/components/cart-page/cart-page.component.spec.ts
@@ -1,16 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CartPageComponent } from './cart-page.component';
+import { CartService } from '../../services/cart/cart.service';
+import { of } from 'rxjs';
+import { CartItemModel } from '../../models/cartItem.model';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 describe('CartPageComponent', () => {
   let component: CartPageComponent;
   let fixture: ComponentFixture<CartPageComponent>;
+  let mockCartService;
+
+  const mockCartItems: CartItemModel[] = [
+    {
+      productId: 1,
+      name: 'Item 1',
+      price: 100,
+      quantity: 2,
+      description: 'Description of Item 1',
+      image: 'url-to-image-1',
+      availableQuantity: 5,
+      userId: 'user123'
+    },
+    {
+      productId: 2,
+      name: 'Item 2',
+      price: 50,
+      quantity: 1,
+      description: 'Description of Item 2',
+      image: 'url-to-image-2',
+      availableQuantity: 10,
+      userId: 'user123'
+    }
+  ];
 
   beforeEach(async () => {
+    mockCartService = {
+      getCartItems: jasmine.createSpy('getCartItems').and.returnValue(of(mockCartItems)),
+      updateCartItem: jasmine.createSpy('updateCartItem').and.returnValue(of(void 0)),
+      removeFromCart: jasmine.createSpy('removeFromCart').and.returnValue(of(void 0)),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CartPageComponent]
-    })
-    .compileComponents();
+      imports: [CommonModule],
+      declarations: [CartPageComponent],
+      providers: [
+        { provide: CartService, useValue: mockCartService },
+        { provide: Router, useValue: {} } 
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartPageComponent);
     component = fixture.componentInstance;
@@ -19,5 +57,17 @@ describe('CartPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load cart items on init', () => {
+    component.ngOnInit();
+    expect(component.products.length).toBe(2);
+    expect(component.products).toEqual(mockCartItems);
+  });
+
+  it('should calculate the correct order total', () => {
+    component.products = mockCartItems;
+    component.updateOrderTotal();
+    expect(component.orderTotal).toBe(250);
   });
 });
