14bb9a57da36925f3e006dbb852c7d0536b8214a
Lucas Freitas <102175151+lcffreitas@users.noreply.github.com>
2024-09-17 07:16:29 -0300

    catalog: added service and models & web fixes
    * catalog: added service and models
    
    * catalog: add view individual product from api
    
    * web: fix page scrolling
    
    * web: reorganized folder structure and updated product display
    
    * web: product-display component renamed to display-highlighted-component
    
    * web: initial product table with static products
    
    * web: added create product, delete product, search by name, create brand, create category
    
    * web: fixed management files location
    
    * web: added update product, update category, update brand
    
    * web: added GetById Brand and GetById Category
    
    * web: layout updates and add product logic
    
    * web: css adjustments, add, update and disable added
    
    * web: search-bar finished
    
    * service: resolved erros and added docker
    
    * service: added docker connection string
    
    * Service: Management files ported to Catalog
    
    * service: removed management from files
    
    * service: removed Dockerfile from eShop.Web and reverted Package-lock changes
    
    * Web/Services: code refinements
    
    ---------
    
    Co-authored-by: Aron <aron.cardoso1@gmail.com>
    Co-authored-by: Victor Tedesco <victor.augusto.tedesco@gmail.com>

--- src/Web/eShop.Web/src/app/app.config.ts
+++ src/Web/eShop.Web/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
