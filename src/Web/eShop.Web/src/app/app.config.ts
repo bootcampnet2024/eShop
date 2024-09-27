2615c971ce62eb3205ac2c743e29b7393cddf723
sofiabucci <soliveirabucci@gmail.com~>
2024-09-27 10:28:16 +0100

    ajustes

--- src/Web/eShop.Web/src/app/app.config.ts
+++ src/Web/eShop.Web/src/app/app.config.ts
@@ -1,15 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimations(),provideClientHydration(),
-    provideHttpClient(withInterceptorsFromDi())
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimations(),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: JWT_OPTIONS,
+      useValue: {}  
+    },
+    JwtHelperService
   ]
 };
