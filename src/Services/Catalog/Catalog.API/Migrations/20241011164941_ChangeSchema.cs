84d6f1aeecbef370597bf47e8f8dd5d38f627b51
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-10-11 13:59:48 -0300

    basket and catalog: add missing schema rename migration

---
+++ src/Services/Catalog/Catalog.API/Migrations/20241011164941_ChangeSchema.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "catalog");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Product",
+                newSchema: "catalog");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Category",
+                newSchema: "catalog");
+
+            migrationBuilder.RenameTable(
+                name: "Brand",
+                newName: "Brand",
+                newSchema: "catalog");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Product",
+                schema: "catalog",
+                newName: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                schema: "catalog",
+                newName: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Brand",
+                schema: "catalog",
+                newName: "Brand");
+        }
+    }
+}
