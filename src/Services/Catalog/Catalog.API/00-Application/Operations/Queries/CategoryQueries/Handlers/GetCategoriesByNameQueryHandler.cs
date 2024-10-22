9eaf70bb039f094f2b563f313cd25a881211919c
Lucas Freitas <lcffreitas1@gmail.com>
2024-10-21 21:47:11 -0300

    core: getcategorybyname and getbrandbyname with tests added

---
+++ src/Services/Catalog/Catalog.API/00-Application/Operations/Queries/CategoryQueries/Handlers/GetCategoriesByNameQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API._01_Services.Models;
+using Catalog.API._01_Services;
+using MediatR;
+
+namespace Catalog.API._00_Application.Operations.Queries.CategoryQueries.Handlers
+{
+    public class GetCategoriesByNameQueryHandler(ICatalogCategoryService categoryService) : IRequestHandler<GetCategoriesByNameQuery, IEnumerable<CatalogCategory>>
+    {
+        private readonly ICatalogCategoryService _categoryService = categoryService;
+        public async Task<IEnumerable<CatalogCategory>> Handle(GetCategoriesByNameQuery request, CancellationToken cancellationToken)
+        {
+            var categories = await _categoryService.GetByName(request.name);
+            return categories;
+        }
+    }
+}
