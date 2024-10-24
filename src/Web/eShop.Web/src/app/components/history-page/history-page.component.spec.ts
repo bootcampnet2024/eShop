60da201a4159dce72d1cee6571b2e7a0d780ca49
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-10-24 10:25:28 -0300

    corrigindo tests

--- src/Web/eShop.Web/src/app/components/history-page/history-page.component.spec.ts
+++ src/Web/eShop.Web/src/app/components/history-page/history-page.component.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { JwtModule, JwtHelperService, JWT_OPTIONS } from "@auth0/angular-jwt";
+import { ActivatedRoute } from "@angular/router";
+import { AuthService } from "../../core/auth/auth.service";
+import { of } from "rxjs";
+import { HistoryPageComponent } from "../history-page/history-page.component";
 
-import { HistoryPageComponent } from './history-page.component';
+const mockActivatedRoute = {
+  params: of({}), 
+};
 
-describe('HistoryPageComponent', () => {
+describe("HistoryPageComponent", () => {
   let component: HistoryPageComponent;
   let fixture: ComponentFixture<HistoryPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HistoryPageComponent]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        JwtModule.forRoot({}),
+        HistoryPageComponent,
+      ],
+      providers: [
+        AuthService,
+        JwtHelperService,
+        {
+          provide: JWT_OPTIONS,
+          useValue: {},
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute, 
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HistoryPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
